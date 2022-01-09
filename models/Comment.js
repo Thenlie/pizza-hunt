fa90e0f6bc9806b4d1efa482d5911edaf8e59e32
Leithen <leithen113@gmail.com>
2022-01-09 10:06:32 -0700

    Added data validation

--- models/Comment.js
+++ models/Comment.js
@@ -7,10 +7,13 @@ const ReplySchema = new Schema({
     default: () => new Types.ObjectId()
   },
   replyBody: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
   },
   writtenBy: {
-    type: String
+    type: String,
+    required: true
   },
   createdAt: {
     type: Date,
@@ -26,10 +29,13 @@ const ReplySchema = new Schema({
 const CommentSchema = new Schema(
   {
     writtenBy: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     commentBody: {
-      type: String
+      type: String,
+      required: true
     },
     createdAt: {
       type: Date,
