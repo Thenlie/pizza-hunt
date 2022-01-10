46a7b1d81ff2935103c26a599bac4c409491cec9
Leithen <leithen113@gmail.com>
2022-01-09 18:40:20 -0700

    Updating mongo creds

--- server.js
+++ server.js
@@ -8,14 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
-  //  auth: {
-  //   user: process.env.MONGO_DB_USER,
-  //   password: process.env.MONGO_DB_PASSWORD
-  // },
-  useNewUrlParser: true,
-  useUnifiedTopology: true  
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {});
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
